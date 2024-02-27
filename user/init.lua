f35873d23bcf2ef2ec0c66496cdb326bf470d4cf
yuhuahung <yuhuahung@synology.com>
2024-02-27 15:28:18 +0800

    feat: replace telescope-live-grep-args with astro community version

--- user/init.lua
+++ user/init.lua
@@ -8,20 +8,7 @@ return {
     { import = "astrocommunity.pack.go"},
     -- community plugins
     { import = "astrocommunity.completion.copilot-lua"},
-    -- core plugins
-    "nvim-telescope/telescope.nvim",
-    {
-      "nvim-telescope/telescope.nvim",
-      dependencies = {
-        "nvim-telescope/telescope-live-grep-args.nvim",
-      },
-      config = function(plugin, opts)
-        require("plugins.configs.telescope")(plugin, opts)
-
-        local telescope = require "telescope"
-        telescope.load_extension "live_grep_args"
-      end,
-    },
+    { import = "astrocommunity.utility.telescope-live-grep-args-nvim"},
   },
   mappings = {
     n = {
