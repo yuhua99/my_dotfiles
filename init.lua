70a2ee31456d25e13a3c0cf17c9f94e31af72e53
yuhuahung <yuhuahung@synology.com>
2024-02-29 11:16:38 +0800

    feat: update telescope live grep args config

--- init.lua
+++ init.lua
@@ -8,7 +8,28 @@ return {
     { import = "astrocommunity.pack.go"},
     -- community plugins
     { import = "astrocommunity.completion.copilot-lua"},
-    { import = "astrocommunity.utility.telescope-live-grep-args-nvim"},
+    -- core plugins
+    "nvim-telescope/telescope.nvim",
+    {
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      config = function()
+        require("telescope").setup {
+          extensions = {
+            live_grep_args = {
+              auto_quoting = true,
+              mappings = {
+                i = {
+                  ["<Tab>"] = require("telescope-live-grep-args.actions").quote_prompt(),
+                  ["<C-i>"] = require("telescope-live-grep-args.actions").quote_prompt({ postfix = " --iglob " }),
+                },
+              },
+            },
+          },
+        }
+
+        require("telescope").load_extension("live_grep_args")
+      end,
+    },
   },
   mappings = {
     n = {
