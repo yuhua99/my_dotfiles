fb779b087bf3c5d98782a5e26bb2758e1f117720
yuhuahung <yuhuahung@synology.com>
2024-07-29 11:33:56 +0800

    feat: add setup script

---
+++ setup.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+declare -a tasks
+tasks=(
+  "gitconfig"
+  "nvim"
+  "tmux"
+)
+
+################
+##  Function  ##
+################
+
+gitconfig() {
+  AskOption ".gitconfig"
+  echo "Remember to change the name and email in .gitconfig file!"
+}
+
+nvim() {
+  AskOption "nvim" "$HOME/.config"
+}
+
+tmux() {
+  AskOption ".tmux.conf"
+}
+
+################
+##    Main    ##
+################
+
+current_path=$(pwd)
+
+CopyToHome()
+{
+  target=$1
+  target_path=$2
+
+
+  if [[ -e $target_path/$target ]]; then
+    echo "File $target_path/$target exists!!"
+    printf "Do you want to overwrite it? [y/n]: "
+    read answer
+    if [[ $answer == "y" ]]; then
+      cp -r $current_path/$target $target_path/
+      echo "Overwrite $target_path/$target successfully!!"
+    else
+      echo "Skip $target"
+    fi
+  else
+    cp -r $current_path/$target $target_path/
+    echo "Copy $target to $target_path/ successfully!!"
+  fi
+}
+
+AskOption()
+{
+  if [[ -n "$1" ]]; then
+    task=$1
+  else
+    echo "[error] Task should be provided!!"
+  fi
+
+  if [[ -n $2 ]]; then
+    target_path=$2
+  else
+    target_path="$HOME"
+  fi
+
+  echo "Please choise an option:"
+  echo "  [1] Apply $task"
+  echo "  [2] Show diff"
+  printf "Enter your choice: "
+  read option
+  case $option in
+    1)
+      CopyToHome $task $target_path
+      ;;
+    2)
+      if command -v delta >/dev/null 2>&1; then
+        echo "Diff between $current_path/$task and $target_path/$task:"
+        delta $current_path/$task $target_path/$task
+      else
+        echo "delta command not found. Please install delta to use this option."
+      fi
+      ;;
+    *)
+      echo "Invalid option!!"
+      exit 1
+      ;;
+  esac
+}
+
+AskTask()
+{
+  if [[ -n "$1" ]]; then
+    Num=$1
+  else
+    echo "Please choice task to do:"
+    for ((num=0; num<${#tasks[@]}; num++)); do
+      echo -e "  [$((num+1))]\t${tasks[$num]}"
+    done
+    printf "Enter the number: "
+    read Num
+  fi
+
+  # Check if the task exists in the tasks array
+  if [[ -n "${tasks[$Num-1]}" ]]; then
+    ${tasks[$Num-1]}
+  else
+    echo "Unknown selection!!"
+    exit 1
+  fi
+}
+
+# If the help option is provided, display help and exit
+if [[ $1 == "-h" ]] || [[ $1 == "--help" ]]; then
+  displayHelp
+  exit 0
+fi
+
+AskTask $1
+
