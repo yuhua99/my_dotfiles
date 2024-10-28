a056dd0b74fd54eaa0d607bd8219fa9ae0968dd1
yuhuahung <yuhuahung@synology.com>
2024-10-28 19:24:34 +0800

    feat(nvim): add copilot and change the complete key to tab

--- nvim/init.lua
+++ nvim/init.lua
@@ -775,7 +775,7 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<Tab>'] = cmp.mapping.confirm { select = true },
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
@@ -821,6 +821,15 @@ require('lazy').setup({
           { name = 'path' },
         },
       }
+    end,
+  },
+
+  {
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup {}
     end,
   },
 
