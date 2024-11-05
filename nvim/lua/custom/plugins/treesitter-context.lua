6ff07d1ba9d6dddf3a7c91688225fd6f32811d63
yuhuahung <yuhuahung@synology.com>
2024-11-05 14:39:57 +0800

    feat(nvim): switch to nvim

--- nvim/lua/custom/plugins/treesitter-context.lua
+++
@@ -1,8 +0,0 @@
--- Show context of the current function
-return {
-  'nvim-treesitter/nvim-treesitter-context',
-  event = 'VeryLazy',
-  opts = function()
-    return { mode = 'cursor', max_lines = 3 }
-  end,
-}
