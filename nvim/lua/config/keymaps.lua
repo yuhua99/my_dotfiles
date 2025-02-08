a0a02fa79aa17885e01be608416cf55fd8ffe347
YOUR_NAME <YOUR_EMAIL>
2025-02-09 02:00:53 +0800

    feat: prvent c from yanking into clipboard

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,3 +6,7 @@ vim.keymap.del("n", "<leader>l") -- lazy
 vim.keymap.del("n", "<leader>L") -- changelog
 vim.keymap.del("n", "<leader>-") -- split window
 vim.keymap.del("n", "<leader>|") -- split window vertically
+
+-- prevent from yanking into the system clipboard
+vim.api.nvim_set_keymap("n", "c", '"_c', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "c", '"_c', { noremap = true, silent = true })
