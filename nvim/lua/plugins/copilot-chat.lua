bf1ef43d7acab65d9a8ba895ac50fec3630f60f3
yuhuahung <yuhuahung@synology.com>
2025-04-07 14:51:55 +0800

    fix(nvim): update nvim and fix copilot lualine issues

--- nvim/lua/plugins/copilot-chat.lua
+++ nvim/lua/plugins/copilot-chat.lua
@@ -1,12 +1,10 @@
 return {
-  { import = "lazyvim.plugins.extras.ai.copilot-chat" },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     opts = function()
       local user = vim.env.USER or "User"
       user = user:sub(1, 1):upper() .. user:sub(2)
       return {
-        auto_insert_mode = true,
         question_header = "  " .. user .. " ",
         answer_header = "  Copilot ",
         window = {
