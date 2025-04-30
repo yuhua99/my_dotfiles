66f4dcfafd4b3c775ae8b404241649d22d6c7bdf
yuhuahung <yuhuahung@synology.com>
2025-04-30 16:18:13 +0800

    feat(nvim): update code companion spinner

--- nvim/lua/plugins/code-companion/spinner.lua
+++ nvim/lua/plugins/code-companion/spinner.lua
@@ -45,8 +45,8 @@ function M:update_spinner()
 
   local last_line = vim.api.nvim_buf_line_count(buf) - 1
   vim.api.nvim_buf_set_extmark(buf, self.namespace_id, last_line, 0, {
-    virt_lines = { { { self.spinner_symbols[self.spinner_index] .. " Processing...", "Comment" } } },
-    virt_lines_above = true, -- false means below the line
+    virt_text = { { self.spinner_symbols[self.spinner_index] .. " Processing...", "Comment" } },
+    virt_text_pos = "right_align",
   })
 end
 
