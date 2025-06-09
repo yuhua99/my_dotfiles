372f76967f9382602c0a606e3344d464c6bd1402
yuhuahung <yuhuahung@synology.com>
2025-06-09 18:33:11 +0800

    feat(nvim): change default code companion model to claude 4

--- nvim/lua/plugins/code-companion.lua
+++ nvim/lua/plugins/code-companion.lua
@@ -54,7 +54,7 @@ return {
           return require("codecompanion.adapters").extend("copilot", {
             schema = {
               model = {
-                default = "gemini-2.5-pro",
+                default = "claude-sonnet-4",
               },
             },
           })
