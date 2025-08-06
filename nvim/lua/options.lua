6db8f0bcb98994cccee7f466a5fd585135ce16e3
yuhuahung <yuhuahung@synology.com>
2025-08-06 18:51:49 +0800

    feat(nvim): tab indent

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -71,4 +71,10 @@ vim.o.scrolloff = 10
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
+-- Set tab width to 2 spaces
+vim.o.tabstop = 2 -- Number of spaces that a <Tab> in the file counts for
+vim.o.shiftwidth = 2 -- Number of spaces to use for each step of (auto)indent
+vim.o.softtabstop = 2 -- Number of spaces that a <Tab> counts for while performing editing operations
+vim.o.expandtab = true -- Use spaces instead of tabs
+
 -- vim: ts=2 sts=2 sw=2 et
