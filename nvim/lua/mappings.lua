109939fef0db7af0ec8790bc272f9df2c03b8c3f
yuhuahung <yuhuahung@synology.com>
2025-08-06 15:20:43 +0800

    feat(nvim): start from nvchad

---
+++ nvim/lua/mappings.lua
@@ -0,0 +1,28 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+local del = vim.keymap.del
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+-- delete default binding
+del("n", "<Tab>")
+del("n", "<S-Tab>")
+del("n", "<C-n>")
+
+-- tabufline
+map("n", "L", function()
+  require("nvchad.tabufline").next()
+end, { desc = "buffer goto next" })
+
+map("n", "H", function()
+  require("nvchad.tabufline").prev()
+end, { desc = "buffer goto prev" })
+
+-- nvimtree
+map("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle" })
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
