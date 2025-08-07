67188cee21ad586ac3bed7ad90c137dcc8cd356e
yuhuahung <yuhuahung@synology.com>
2025-08-07 16:21:01 +0800

    feat(nvim): add yank highlighting

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -9,3 +9,12 @@ vim.api.nvim_create_autocmd("BufEnter", {
     end
   end,
 })
+
+-- Highlight yanked text
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
