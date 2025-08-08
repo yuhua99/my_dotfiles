2cfad785578f933d4e90bfff4df4e38898226b75
yuhuahung <yuhuahung@synology.com>
2025-08-08 13:52:33 +0800

    feat(nvim): config vue and ts lsp

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,27 @@
 require("nvchad.configs.lspconfig").defaults()
+require("mason").setup {
+  ensure_installed = {
+    "lua-language-server",
+    "vue-language-server",
+    "typescript-language-server",
+  },
+}
 
-local servers = { "html", "cssls" }
+local vue_ls_path = vim.fn.expand "$MASON/packages/vue-language-server"
+local vue_plugin_path = vue_ls_path .. "/node_modules/@vue/language-server"
+
+vim.lsp.config("ts_ls", {
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = vue_plugin_path,
+        languages = { "vue" },
+      },
+    },
+  },
+  filetypes = { "typescript", "javascript", "vue" },
+})
+
+local servers = { "ts_ls", "vue_ls" }
 vim.lsp.enable(servers)
-
--- read :h vim.lsp.config for changing options of lsp servers 
