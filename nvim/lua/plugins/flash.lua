41a7696c249281a6fcf58e5e048432c00b200f1f
yuhuahung <yuhuahung@synology.com>
2025-08-08 14:48:57 +0800

    feat(nvim): custom flash label color

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -3,6 +3,12 @@ return {
   event = "VeryLazy",
   ---@type Flash.Config
   opts = {},
+  config = function(_, opts)
+    require("flash").setup(opts)
+    vim.api.nvim_set_hl(0, "FlashLabel", {
+      link = "Special",
+    })
+  end,
   -- stylua: ignore
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
