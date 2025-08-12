5566ce1b7939be2e6cddecf6f16cdae20619360e
yuhuahung <yuhuahung@synology.com>
2025-08-12 16:36:32 +0800

    chore(nvim): clean up

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -2,7 +2,7 @@ local map = vim.keymap.set
 map('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+map('n', '<leader>cd', vim.diagnostic.setloclist, { desc = 'Diagnostic quickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -10,7 +10,7 @@ return {
           require('conform').format { async = true, lsp_format = 'fallback' }
         end,
         mode = '',
-        desc = 'format',
+        desc = 'Format',
       },
     },
     opts = {
