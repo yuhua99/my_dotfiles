fda07a485058afb5079d68664764c9b287421f2f
yuhuahung <yuhuahung@synology.com>
2025-08-12 15:59:26 +0800

    feat(nvim): add file picker

---
+++ nvim/lua/plugins/picker.lua
@@ -0,0 +1,65 @@
+return {
+  {
+    'ibhagwan/fzf-lua',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+      fzf_opts = { ['--layout'] = 'default' },
+      winopts = {
+        height = 0.8, -- window height
+        width = 0.8, -- window width
+        row = 0.5,
+        border = 'single',
+        title_pos = 'left', -- 'left', 'center' or 'right'
+        preview = {
+          border = 'single', -- preview border: accepts both `nvim_open_win`
+          title_pos = 'left', -- left|center|right, title alignment
+        },
+      },
+    },
+    keys = {
+      { '<leader>/', '<cmd>FzfLua live_grep<cr>', desc = 'Live grep' },
+      { '<leader>fw', '<cmd>FzfLua live_grep<cr>', desc = 'Live grep' },
+      { '<leader>fb', '<cmd>FzfLua buffers<cr>', desc = 'Find buffers' },
+      { '<leader>fs', '<cmd>FzfLua grep_cword<cr>', desc = 'Search word under cursor' },
+      { '<leader>gs', '<cmd>FzfLua git_status<cr>', desc = 'Git status' },
+      { '<leader>gc', '<cmd>FzfLua git_commits<cr>', desc = 'Git commits' },
+      { '<leader>cr', '<cmd>FzfLua lsp_references<cr>', desc = 'LSP references' },
+      { '<leader>cd', '<cmd>FzfLua lsp_definitions<cr>', desc = 'LSP definitions' },
+      { '<leader>cs', '<cmd>FzfLua lsp_document_symbols<cr>', desc = 'Document symbols' },
+      { '<leader>fh', '<cmd>FzfLua helptags<cr>', desc = 'Help tags' },
+    },
+  },
+  {
+    'dmtrKovalenko/fff.nvim',
+    build = 'cargo build --release',
+    opts = {
+      prompt = '> ',
+      layout = {
+        prompt_position = 'bottom',
+      },
+    },
+    keys = {
+      {
+        '<leader> ',
+        function()
+          require('fff').find_files()
+        end,
+        desc = 'Open file picker',
+      },
+      {
+        '<leader>ff',
+        function()
+          require('fff').find_files()
+        end,
+        desc = 'Open file picker',
+      },
+      {
+        '<leader>gf',
+        function()
+          require('fff').find_in_git_root()
+        end,
+        desc = 'Git files',
+      },
+    },
+  },
+}
