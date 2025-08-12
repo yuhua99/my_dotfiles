1853c27067bd074490ccffaff0546d697155bf14
yuhuahung <yuhuahung@synology.com>
2025-08-12 17:59:53 +0800

    feat(nvim): vue lsp

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -6,7 +6,6 @@ return {
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
       -- Mason must be loaded before its dependents so we need to set it up here.
-      -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
       { 'mason-org/mason.nvim', opts = {} },
 
       -- Useful status updates for LSP.
@@ -129,35 +128,30 @@ return {
       --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-      local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
 
-        lua_ls = {
-          -- cmd = { ... },
-          -- filetypes = { ... },
-          -- capabilities = {},
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
+      local vue_ls_path = vim.fn.expand '$MASON/packages/vue-language-server'
+      local vue_plugin_path = vue_ls_path .. '/node_modules/@vue/language-server'
+
+      vim.lsp.config('ts_ls', {
+        init_options = {
+          plugins = {
+            {
+              name = '@vue/typescript-plugin',
+              location = vue_plugin_path,
+              languages = { 'vue' },
             },
           },
         },
+        filetypes = { 'typescript', 'javascript', 'vue' },
+      })
+
+      local servers = {
+        'lua_ls',
+        'vue_ls',
+        'vtsls',
       }
+
+      vim.lsp.enable(servers)
     end,
   },
 }
