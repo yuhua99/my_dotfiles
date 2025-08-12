ed196149bc576895d9844fb33ce99067ca4f8737
yuhuahung <yuhuahung@synology.com>
2025-08-12 17:47:49 +0800

    feat(nvim): auto session

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -1,7 +1,32 @@
 return {
   {
     'folke/persistence.nvim',
-    event = 'BufReadPre', -- this will only start session saving when an actual file was opened
-    opts = {},
+    event = 'VimEnter',
+    opts = {
+      options = { 'buffers', 'curdir', 'tabpages', 'winsize', 'help', 'globals', 'skiprtp' },
+    },
+    config = function(_, opts)
+      require('persistence').setup(opts)
+
+      -- Auto-restore session when starting nvim without arguments
+      vim.api.nvim_create_autocmd('VimEnter', {
+        group = vim.api.nvim_create_augroup('persistence_auto_restore', { clear = true }),
+        callback = function()
+          -- Only restore if nvim was started without arguments and no stdin
+          if vim.fn.argc(-1) == 0 and not vim.g.started_with_stdin then
+            require('persistence').load()
+          end
+        end,
+        nested = true,
+      })
+
+      -- Track if started with stdin
+      vim.api.nvim_create_autocmd('StdinReadPre', {
+        group = vim.api.nvim_create_augroup('persistence_stdin_check', { clear = true }),
+        callback = function()
+          vim.g.started_with_stdin = true
+        end,
+      })
+    end,
   },
 }
