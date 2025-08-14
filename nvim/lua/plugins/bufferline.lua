285dcc81d352949c826a744583d75ecd16815576
yuhuahung <yuhuahung@synology.com>
2025-08-14 14:06:01 +0800

    fix(nvim): exclude oil and health check from buffer

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -6,6 +6,15 @@ return {
   opts = {
     options = {
       always_show_bufferline = false,
+      custom_filter = function(buf_number)
+        local buf_name = vim.fn.bufname(buf_number)
+        local buf_ft = vim.fn.getbufvar(buf_number, '&filetype')
+        -- Exclude oil buffers and health check buffers
+        if buf_ft == 'oil' or buf_ft == 'checkhealth' or string.match(buf_name, 'oil://') then
+          return false
+        end
+        return true
+      end,
     },
   },
   keys = {
