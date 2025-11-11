01598cb27a0960779e5f1ca5d3c5cb6e85c2c672
yuhuahung <yuhuahung@synology.com>
2025-11-11 11:30:48 +0800

    feat(nvim): fzf as gd

--- nvim/lua/plugins/picker.lua
+++ nvim/lua/plugins/picker.lua
@@ -9,6 +9,18 @@ return {
             syntax_limit_b = 1024 * 100, -- 100KB
           },
         },
+        grep = {
+          rg_glob = true,
+          -- first returned string is the new search query
+          -- second returned string are (optional) additional rg flags
+          -- @return string, string?
+          -- e.g. hello -- -tts will search for hello strings in ts files
+          rg_glob_fn = function(query, opts)
+            local regex, flags = query:match '^(.-)%s%-%-(.*)$'
+            -- If no separator is detected will return the original query
+            return (regex or query), flags
+          end,
+        },
       }
       -- fzf-lua as vim.ui.select interface
       require('fzf-lua').register_ui_select()
@@ -28,6 +40,10 @@ return {
       { '<leader>cs', '<cmd>FzfLua lsp_document_symbols<cr>', desc = 'Document symbols' },
       { '<leader>fh', '<cmd>FzfLua helptags<cr>', desc = 'Help tags' },
       { '<leader>gf', '<cmd>FzfLua git_files<cr>', desc = 'Git files' },
+      -- Override default LSP keymaps
+      { 'gd', '<cmd>FzfLua lsp_definitions<cr>', desc = 'Go to definition' },
+      { 'gR', '<cmd>FzfLua lsp_references<cr>', desc = 'Go to references' },
+      { 'gi', '<cmd>FzfLua lsp_implementations<cr>', desc = 'Go to implementation' },
     },
   },
 }
