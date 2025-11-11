25e7cb1a20e71795c7d5d17e64959fbae87f631b
yuhuahung <yuhuahung@synology.com>
2025-11-11 11:12:10 +0800

    feat(nvim): use fzf file picker

--- nvim/lua/plugins/picker.lua
+++ nvim/lua/plugins/picker.lua
@@ -2,65 +2,21 @@ return {
   {
     'ibhagwan/fzf-lua',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {
-      fzf_opts = { ['--layout'] = 'default' },
-      winopts = {
-        height = 0.8, -- window height
-        width = 0.8, -- window width
-        row = 0.5,
-        border = 'single',
-        title_pos = 'left', -- 'left', 'center' or 'right'
-        preview = {
-          border = 'single', -- preview border: accepts both `nvim_open_win`
-          title_pos = 'left', -- left|center|right, title alignment
-        },
-      },
-    },
     keys = {
       { 'fw', '<cmd>FzfLua live_grep<cr>', desc = 'Live grep' },
+      { 'ff', '<cmd>FzfLua files<cr>', desc = 'Open file picker' },
       { '<leader>fb', '<cmd>FzfLua buffers<cr>', desc = 'Find buffers' },
       { '<leader>fr', '<cmd>FzfLua resume<cr>', desc = 'Resume' },
       { '<leader>fs', '<cmd>FzfLua grep_cword<cr>', desc = 'Search word under cursor' },
       { '<leader>fw', '<cmd>FzfLua live_grep<cr>', desc = 'Live grep' },
+      { '<leader>ff', '<cmd>FzfLua files<cr>', desc = 'Open file picker' },
       { '<leader>gs', '<cmd>FzfLua git_status<cr>', desc = 'Git status' },
       { '<leader>gc', '<cmd>FzfLua git_commits<cr>', desc = 'Git commits' },
       { '<leader>cR', '<cmd>FzfLua lsp_references<cr>', desc = 'Fzf references' },
       { '<leader>cD', '<cmd>FzfLua lsp_definitions<cr>', desc = 'Fzf definitions' },
       { '<leader>cs', '<cmd>FzfLua lsp_document_symbols<cr>', desc = 'Document symbols' },
       { '<leader>fh', '<cmd>FzfLua helptags<cr>', desc = 'Help tags' },
-    },
-  },
-  {
-    'dmtrKovalenko/fff.nvim',
-    build = 'cargo build --release',
-    opts = {
-      prompt = '> ',
-      layout = {
-        prompt_position = 'bottom',
-      },
-    },
-    keys = {
-      {
-        'ff',
-        function()
-          require('fff').find_files()
-        end,
-        desc = 'Open file picker',
-      },
-      {
-        '<leader>ff',
-        function()
-          require('fff').find_files()
-        end,
-        desc = 'Open file picker',
-      },
-      {
-        '<leader>gf',
-        function()
-          require('fff').find_in_git_root()
-        end,
-        desc = 'Git files',
-      },
+      { '<leader>gf', '<cmd>FzfLua git_files<cr>', desc = 'Git files' },
     },
   },
 }
