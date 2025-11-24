68546ee09ef1ca4444087f6cd1fa01fd2dc0aeb7
yuhuahung <yuhuahung@synology.com>
2025-11-24 22:18:23 +0800

    feat(fish): remove atuin

--- fish/config.fish
+++ fish/config.fish
@@ -4,11 +4,11 @@ end
 
 fish_add_path --global $HOME/.cargo/bin
 fish_add_path --global $HOME/.local/bin
+fish_add_path --global $HOME/.limbo
 fish_add_path --global /opt/homebrew/bin
 fish_add_path --global /usr/local/bin
 
 # init plugins
-atuin init fish | source
 starship init fish | source
 zoxide init fish | source
 fnm env --use-on-cd --shell fish | source
