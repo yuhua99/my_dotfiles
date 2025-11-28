ddea386e85383cc0bf414eff712b5f6126f02333
yuhuahung <yuhuahung@synology.com>
2025-11-28 13:54:57 +0800

    feat(nvim): change session plugin

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -1,9 +1,13 @@
 return {
   {
-    'olimorris/persisted.nvim',
+    'rmagatti/auto-session',
     lazy = false,
+
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
     opts = {
-      autoload = true,
+      suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
     },
   },
 
