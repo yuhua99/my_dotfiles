f69efc28f011df4e38aee69dcc9bfafe2a4b6dbd
yuhuahung <yuhuahung@synology.com>
2025-12-23 16:36:05 +0800

    chore(nvim): update ai model

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -45,7 +45,7 @@ return {
             return require('codecompanion.adapters').extend('copilot', {
               schema = {
                 model = {
-                  default = 'gemini-3-pro-preview',
+                  default = 'claude-sonnet-4.5',
                 },
               },
             })
