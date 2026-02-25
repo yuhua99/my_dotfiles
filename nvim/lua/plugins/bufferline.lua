4ed2dcf756322d5860f7f4e619952981daa104ae
yuhuahung <yuhuahung@synology.com>
2026-02-25 14:18:04 +0800

    feat(nvim): restore recent closed buffer

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,3 +1,37 @@
+local recently_closed = {}
+
+local function track_current_buffer()
+  local buf = vim.api.nvim_get_current_buf()
+  local name = vim.api.nvim_buf_get_name(buf)
+  local bt = vim.bo[buf].buftype
+
+  if name == '' or bt ~= '' then
+    return
+  end
+
+  table.insert(recently_closed, {
+    path = name,
+    cursor = vim.api.nvim_win_get_cursor(0),
+  })
+end
+
+local function close_buffer_and_track()
+  track_current_buffer()
+  vim.cmd 'BufDel'
+end
+
+local function restore_last_closed_buffer()
+  local entry = table.remove(recently_closed)
+
+  if not entry then
+    vim.notify('No recently closed buffer to restore', vim.log.levels.INFO)
+    return
+  end
+
+  vim.cmd.edit(vim.fn.fnameescape(entry.path))
+  pcall(vim.api.nvim_win_set_cursor, 0, entry.cursor)
+end
+
 return {
   'akinsho/bufferline.nvim',
   version = '*',
@@ -23,8 +57,9 @@ return {
   keys = {
     { 'H', '<cmd> BufferLineCyclePrev <CR>', mode = 'n' },
     { 'L', '<cmd> BufferLineCycleNext <CR>', mode = 'n' },
-    { '<leader>x', '<cmd> BufDel <CR>', desc = 'Close buffer', mode = 'n' },
+    { '<leader>x', close_buffer_and_track, desc = 'Close buffer', mode = 'n' },
     { '<leader>bo', '<cmd> BufferLineCloseOthers <CR>', desc = 'Close all other buffers', mode = 'n' },
-    { '<leader>bx', '<cmd> BufDel <CR>', desc = 'Close buffer', mode = 'n' },
+    { '<leader>bx', close_buffer_and_track, desc = 'Close buffer', mode = 'n' },
+    { '<leader>br', restore_last_closed_buffer, desc = 'Restore recently closed buffer', mode = 'n' },
   },
 }
