2a806e03cfb26a752f52770c203f87fadd359846
yuhuahung <yuhuahung@synology.com>
2026-03-14 23:56:44 +0800

    feat(tmux): tmux ls with t

--- fish/completions/t.fish
+++ fish/completions/t.fish
@@ -1,2 +1,3 @@
 complete -c t -e
+complete -c t -f -n 'test (count (commandline -opc)) -eq 1' -a 'ls' -d 'list tmux sessions'
 complete -c t -f -n 'test (count (commandline -opc)) -eq 1' -a '(tmux list-sessions -F "#{session_name}" 2>/dev/null)' -d 'tmux session'
--- fish/functions/t.fish
+++ fish/functions/t.fish
@@ -1,3 +1,8 @@
 function t --description 'tmux new/attach session'
+    if test (count $argv) -eq 1; and test "$argv[1]" = ls
+        tmux list-sessions
+        return
+    end
+
     tmux new -A -s $argv
 end
